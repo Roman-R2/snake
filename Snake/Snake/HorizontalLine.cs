d051b2a8710b718c75da896db12ebea9ed1aa845
Roman Slyusar <slyusarrv@yandex.ru>
2020-04-02 15:50:34 +0300

    Add some classes adn draw snake

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine : Figure
+    {
+        public HorizontalLine(int xLeft, int xRight, int y, char symbol) {
+            pList = new List<Point>();
+
+            for(int x = xLeft; x <= xRight; x++) {
+                Point p = new Point(x, y, symbol);
+                pList.Add(p);
+            }
+        }
+
+    }
+}
---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+
+        public int x;
+        public int y;
+        public char symbol;
+        private Point tail;
+
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            symbol = p.symbol;
+        }
+
+        public Point(int _x, int _y, char _symbol) {
+            x = _x;
+            y = _y;
+            symbol = _symbol;
+        }
+
+
+        public void Move(int offset, Direction direction) {
+            if (direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y = y - offset;
+            }
+            else if (direction == Direction.DOWN) {
+                y = y + offset;
+            }
+        }
+
+
+        public void Draw() {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+
+    }
+}
